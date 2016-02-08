ec35df110bc8a26bdf198adb5a8b29a621615562
Tyler Johnson <tyler@vintyge.com>
2016-02-07 19:13:02 -0700

    exit if there are no git changes

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,27 @@ then
 	exit 0
 fi
 
+# prep the access key
+eval "$(ssh-agent -s)" # start the ssh agent
 echo "$DOKKU_PRIVATE_KEY" > deploy_key.pem
 chmod 600 deploy_key.pem
 ssh-add deploy_key.pem
+
+# clone the existing dokku repo
 git clone dokku@$DOKKU_HOST:$DOKKU_APPNAME deploy
 cd deploy
+
+# build a new copy in the repo
 npm run clean
 cd ..
 $BIN/superfast --boring pack deploy -y
 cd deploy
+
+# exit if there are no changes
+if git diff --quiet
+then
+	echo "No changes to deploy to production."
+	exit 0
+fi
+
 git status

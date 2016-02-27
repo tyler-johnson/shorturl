fd62d373367b21a10e5c45b427edf41036d9e0b9
Tyler Johnson <tyler@tylerjohnson.me>
2016-02-26 18:46:04 -0700

    chore: started full package

--- Makefile
+++ Makefile
@@ -4,14 +4,14 @@ SRC = $(wildcard src/* src/*/*)
 build: index.js cli.js client.js client.css
 
 index.js: src/index.js $(SRC)
-	$(BIN)/rollup $< -c -f cjs > $@
+	TARGET=node $(BIN)/rollup $< -c -f cjs > $@
 
 cli.js: src/cli.js $(SRC)
 	echo "#!/usr/bin/env node" > $@
-	$(BIN)/rollup $< -c -f cjs >> $@
+	TARGET=node $(BIN)/rollup $< -c -f cjs >> $@
 
 client.js: src/client/index.html
-	$(BIN)/rollup $< -c -f cjs > $@
+	TARGET=browser $(BIN)/rollup $< -c -f cjs > $@
 
 client.css: src/client/index.scss
 	$(BIN)/node-sass --importer build/sass-module-importer.js $< | $(BIN)/postcss --use autoprefixer --autoprefixer.browsers "ie >= 8,> 1%" | $(BIN)/cleancss > $@
--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,41 @@
 import babel from "rollup-plugin-babel";
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import Temple from "templejs";
 import path from "path";
 
+const plugins = [];
+
+if (process.env.TARGET === "browser") {
+	plugins.push({
+		transform: function(source, id) {
+			if (path.extname(id) !== ".html") return;
+
+			return {
+				code: `import Temple from "templejs";\n\n` + Temple.compile(source, {
+					exports: "es6"
+				}),
+				map: { mappings: "" }
+			};
+		}
+	},
+
+	nodeResolve({
+		jsnext: false,
+		main: true,
+		browser: true
+	}),
+
+	commonjs({
+		exclude: [ "src/**" ]
+	}));
+}
+
+plugins.push(babel({
+	exclude: 'node_modules/**'
+}));
+
 export default {
 	onwarn: ()=>{},
-	plugins: [
-		{
-			transform: function(source, id) {
-				if (path.extname(id) !== ".html") return;
-
-				return {
-					code: `import Temple from "templejs";\n\n` + Temple.compile(source, {
-						exports: "es6"
-					}),
-					map: { mappings: "" }
-				};
-			}
-		},
-		babel({
-			exclude: 'node_modules/**'
-		})
-	]
+	plugins: plugins
 };
